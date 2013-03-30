05eb6d6ee5fc88e6ba2d9294949cc415c17a8ef3
Brooke Kuhlmann <brooke@redalchemist.com>
2013-03-30 08:41:29 -0600

    Added the Sublime Text 2 - Gist extension.

--- scripts/extensions.sh
+++ scripts/extensions.sh
@@ -10,6 +10,9 @@ install_git_app "$RAILS_PANEL_EXTENSION_URL" "$RAILS_PANEL_EXTENSION_PATH"
 # Sublime Text 2 - Git Gutter
 install_git_app "$GIT_GUTTER_EXTENSION_URL" "$GIT_GUTTER_EXTENSION_PATH"
 
+# Sublime Text 2 - Gist
+install_git_app "$GIST_EXTENSION_URL" "$GIST_EXTENSION_PATH"
+
 # Sublime Text 2 - Advanced New File
 install_git_app "$ADVANCED_NEW_FILE_EXTENSION_URL" "$ADVANCED_NEW_FILE_EXTENSION_PATH"
 

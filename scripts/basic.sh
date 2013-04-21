fbb3e5544ac63a435d80eef11e32fe7b9832a79e
Brooke Kuhlmann <brooke@redalchemist.com>
2013-04-21 09:44:31 -0600

    Added default desktop background image customization.

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -10,6 +10,9 @@ sudo scutil --set HostName $SYSTEM_NAME
 sudo scutil --set LocalHostName $SYSTEM_NAME
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $SYSTEM_NAME
 
+echo "Setting default desktop image (3200x2000@300)."
+sudo cp settings/default_desktop.jpg /System/Library/CoreServices/DefaultDesktop.jpg
+
 echo "Cleaning default directories..."
 rm -rf $HOME/Documents/*
 rm -rf $HOME/Downloads/*

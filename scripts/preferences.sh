ebb791d1ba2c50f66fe96f267feca9b534c2555c
Brooke Kuhlmann <brooke@redalchemist.com>
2013-05-19 13:24:21 -0600

    Added iTerm and Sublime Text preference support.

--- scripts/preferences.sh
+++ scripts/preferences.sh
@@ -19,3 +19,17 @@ cd ruby_gem_setup
 ./run.sh g
 cd ..
 rm -rf ruby_gem_setup
+
+# iTerm
+git clone git://github.com/bkuhlmann/iterm.git
+cd iterm
+./run.sh i
+cd ..
+rm -rf iterm
+
+# Sublime Text
+git clone git://github.com/bkuhlmann/sublime_text.git
+cd sublime_text
+./run.sh l
+cd ..
+rm -rf sublime_text

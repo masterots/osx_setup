61dd88cd1f722f593450d8496923023cf7a1b0c4
Brooke Kuhlmann <brooke@redalchemist.com>
2013-05-19 16:25:10 -0600

    Extracted Homebrew application setup to the setup.sh script.

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -52,13 +52,12 @@ brew install node
 # Phantom.js
 brew install phantomjs
 
-# Ruby
+# rbenv
 brew install rbenv
 brew install --HEAD ruby-build
 brew install rbenv-gem-rehash
 brew install rbenv-default-gems
 brew install rbenv-vars
-mkdir -p $HOME/.rbenv && cp settings/rbenv-vars.txt $HOME/.rbenv/vars
 mkdir -p $HOME/.rbenv/plugins
 git clone https://github.com/ianheggie/rbenv-binstubs.git $HOME/.rbenv/plugins/rbenv-binstubs
 rbenv install $RUBY_VERSION
@@ -69,21 +68,12 @@ curl get.pow.cx | sh
 
 # Redis
 brew install redis
-cp /usr/local/Cellar/redis/*/homebrew.mxcl.redis.plist $HOME/Library/LaunchAgents/
-launchctl load -w $HOME/Library/LaunchAgents/homebrew.mxcl.redis.plist
 
 # PostgreSQL
 brew install postgresql
-initdb /usr/local/var/postgres -E utf8
-cp /usr/local/Cellar/postgresql/*/homebrew.mxcl.postgresql.plist $HOME/Library/LaunchAgents
-launchctl load -w $HOME/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 
 # MySQL
 brew install mysql
-ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
-/usr/local/opt/mysql/bin/mysql_secure_installation
 
 # Wry
 brew install wry
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,20 @@
 # DESCRIPTION
 # Sets up and launched (if necessary) installed software.
 
+# rbenv vars
+mkdir -p $HOME/.rbenv && cp settings/rbenv-vars.txt $HOME/.rbenv/vars
+
+# Redis
+cp /usr/local/Cellar/redis/*/homebrew.mxcl.redis.plist $HOME/Library/LaunchAgents/
+launchctl load -w $HOME/Library/LaunchAgents/homebrew.mxcl.redis.plist
+
+# PostgreSQL
+initdb /usr/local/var/postgres -E utf8
+cp /usr/local/Cellar/postgresql/*/homebrew.mxcl.postgresql.plist $HOME/Library/LaunchAgents
+launchctl load -w $HOME/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+
+# MySQL
+ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
+/usr/local/opt/mysql/bin/mysql_secure_installation

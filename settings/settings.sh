e200940289864d4e76112e228b98fc5181f8d11f
Brooke Kuhlmann <brooke@redalchemist.com>
2013-08-21 21:13:37 -0600

    Upgraded to Firefox 23.0.1.

--- settings/settings.sh
+++ settings/settings.sh
@@ -108,8 +108,8 @@ export CANARY_APP_URL=https://storage.googleapis.com/chrome-canary
 export CANARY_APP_FILE=GoogleChromeCanary.dmg
 export CANARY_APP_NAME="Google Chrome Canary.app"
 
-export FIREFOX_APP_URL=http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/22.0/mac/en-US
-export FIREFOX_APP_FILE=Firefox%2022.0.dmg
+export FIREFOX_APP_URL=http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/23.0.1/mac/en-US
+export FIREFOX_APP_FILE=Firefox%2023.0.1.dmg
 export FIREFOX_APP_NAME=Firefox.app
 
 export OPERA_APP_URL=http://get.geo.opera.com/pub/opera/desktop/15.0.1147.132/mac

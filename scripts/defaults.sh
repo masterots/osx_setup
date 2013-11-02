2d89a587053e3082d386a525dc08c17ff936ffbd
Brooke Kuhlmann <brooke@redalchemist.com>
2013-11-02 15:56:12 -0600

    Updated Mail.app defaults to disable send and reploy animations.

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -144,6 +144,12 @@ defaults write com.google.Chrome DisablePrintPreview -boolean true
 echo "Mail - Copy email addresses as 'foo@example.com' instead of 'Foo Bar <foo@example.com>'"
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
+echo "Mail - Disable send animation"
+defaults write com.apple.mail DisableSendAnimations -bool true
+
+echo "Mail - Disable reply animation"
+defaults write com.apple.mail DisableReplyAnimations -bool true
+
 echo "Address Book - Enable debug menu"
 defaults write com.apple.addressbook ABShowDebugMenu -bool true
 

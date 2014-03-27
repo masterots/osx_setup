8b510de524381bc6410cc332586c52a1297e9d80
Brooke Kuhlmann <brooke@redalchemist.com>
2014-03-27 06:36:59 -0600

    Updated to Cloak 2.0.1.

--- settings/settings.sh
+++ settings/settings.sh
@@ -221,7 +221,7 @@ export OPEN_OFFICE_APP_FILE=Apache_OpenOffice_4.0.1_MacOS_x86_install_en-US.dmg
 export OPEN_OFFICE_APP_NAME=OpenOffice.app
 
 export CLOAK_APP_URL=https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release
-export CLOAK_APP_FILE=Cloak-1.2.3.dmg
+export CLOAK_APP_FILE=Cloak-2.0.1.dmg
 export CLOAK_APP_NAME=Cloak.app
 
 export SCREENHERO_APP_URL=http://dl.screenhero.com/update/screenhero

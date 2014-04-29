70a099b28b62969e7bce6a51f5ce857a20107c47
Brooke Kuhlmann <brooke@redalchemist.com>
2014-04-29 09:30:42 -0600

    Updated to OpenOffice 4.1.0.

--- settings/settings.sh
+++ settings/settings.sh
@@ -223,8 +223,8 @@ export CHEATSHEET_APP_URL=http://mediaatelier.com/CheatSheet
 export CHEATSHEET_APP_FILE=CheatSheet_1.2.2.zip
 export CHEATSHEET_APP_NAME=CheatSheet.app
 
-export OPEN_OFFICE_APP_URL=http://hivelocity.dl.sourceforge.net/project/openofficeorg.mirror/4.0.1/binaries/en-US
-export OPEN_OFFICE_APP_FILE=Apache_OpenOffice_4.0.1_MacOS_x86_install_en-US.dmg
+export OPEN_OFFICE_APP_URL=http://hivelocity.dl.sourceforge.net/project/openofficeorg.mirror/4.1.0/binaries/en-US
+export OPEN_OFFICE_APP_FILE=Apache_OpenOffice_4.1.0_MacOS_x86-64_install_en-US.dmg
 export OPEN_OFFICE_APP_NAME=OpenOffice.app
 
 export CLOAK_APP_URL=https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release

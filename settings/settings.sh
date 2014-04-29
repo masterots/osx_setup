67ce298465b298b192ba8b04c57189e1cb382908
Brooke Kuhlmann <brooke@redalchemist.com>
2014-04-29 09:37:49 -0600

    Updated to Hazel 3.2.7.

--- settings/settings.sh
+++ settings/settings.sh
@@ -260,7 +260,7 @@ export AIR_DISPLAY_APP_FILE=AirDisplayInstaller.zip
 export AIR_DISPLAY_APP_NAME="Air Display Host.app"
 
 export HAZEL_APP_URL=https://s3.amazonaws.com/Noodlesoft
-export HAZEL_APP_FILE=Hazel-3.2.5.dmg
+export HAZEL_APP_FILE=Hazel-3.2.7.dmg
 export HAZEL_APP_NAME=Hazel.prefPane
 
 export TRAILER_APP_URL=http://dev.housetrip.com/trailer

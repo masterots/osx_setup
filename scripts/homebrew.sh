d1cd21b8b7fa0ea0f705f137724009331de3ee52
Brooke Kuhlmann <brooke@redalchemist.com>
2014-09-27 17:39:15 -0600

    Updated Homebrew install location.

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -5,7 +5,7 @@
 
 # EXECUTION
 # Homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 export PATH="/usr/local/bin:$PATH"
 printf "export PATH=\"/usr/local/bin:$PATH\"\n" >> $HOME/.bash_profile
 

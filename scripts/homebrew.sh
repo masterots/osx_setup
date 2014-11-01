e9c9d91ae4bc149af6d7defdca01a75ccb439ce7
Brooke Kuhlmann <brooke@alchemists.io>
2014-11-01 12:08:49 -0600

    Fixed Homebrew install guard.

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -5,7 +5,7 @@
 
 # EXECUTION
 # Homebrew
-if command -v brew > /dev/null; then
+if ! command -v brew > /dev/null; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   export PATH="/usr/local/bin:$PATH"
   printf "export PATH=\"/usr/local/bin:$PATH\"\n" >> $HOME/.bash_profile

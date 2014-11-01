d93d87cbc1e7d558365f271a7b4828e038e92cd2
Brooke Kuhlmann <brooke@alchemists.io>
2014-11-01 12:03:16 -0600

    Added previously installed brew guard.

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -5,9 +5,11 @@
 
 # EXECUTION
 # Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-export PATH="/usr/local/bin:$PATH"
-printf "export PATH=\"/usr/local/bin:$PATH\"\n" >> $HOME/.bash_profile
+if command -v brew > /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  export PATH="/usr/local/bin:$PATH"
+  printf "export PATH=\"/usr/local/bin:$PATH\"\n" >> $HOME/.bash_profile
+fi
 
 # Readline
 brew install readline

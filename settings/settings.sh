285d25ace4f294b298c3ccfa1af2cbc4f6e3a269
Brooke Kuhlmann <brooke@alchemists.io>
2015-01-04 09:07:41 -0700

    Updated to VirtualBox 4.3.20.

--- settings/settings.sh
+++ settings/settings.sh
@@ -245,7 +245,7 @@ export SCREENHERO_APP_NAME=Screenhero.app
 export SCREENHERO_APP_URL="http://dl.screenhero.com/update/screenhero/Screenhero.dmg"
 
 export VIRTUAL_BOX_APP_NAME=VirtualBox.app
-export VIRTUAL_BOX_APP_URL="http://download.virtualbox.org/virtualbox/4.3.18/VirtualBox-4.3.18-96516-OSX.dmg"
+export VIRTUAL_BOX_APP_URL="http://download.virtualbox.org/virtualbox/4.3.20/VirtualBox-4.3.20-96996-OSX.dmg"
 
 export DOXIE_APP_NAME=Doxie.app
 export DOXIE_APP_URL="http://www.getdoxie.com/resources/files/download_current_mac.php"

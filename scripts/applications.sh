185f3e25cf6d7b8f9ad700be2711dc15b8b9cc8e
Brooke Kuhlmann <brooke@alchemists.io>
2015-03-19 20:41:32 -0600

    Updated to VLC 2.2.0.

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -68,7 +68,7 @@ install_dmg_app "$DOUBLETAKE_APP_URL" "DoubleTake" "$DOUBLETAKE_APP_NAME"
 install_tar_app "$IMAGE_OPTIM_APP_URL" "$IMAGE_OPTIM_APP_NAME" "-xjf"
 
 # VLC
-install_dmg_app "$VLC_APP_URL" "vlc-2.1.5" "$VLC_APP_NAME"
+install_dmg_app "$VLC_APP_URL" "vlc-2.2.0" "$VLC_APP_NAME"
 
 # PG Admin
 install_dmg_app "$PG_ADMIN_APP_URL" "pgAdmin3" "$PG_ADMIN_APP_NAME"

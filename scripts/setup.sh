ddd3fedd49a2c9e3229377a2cde3c3ce64072c3a
Brooke Kuhlmann <brooke@alchemists.io>
2015-03-21 08:48:54 -0600

    Added basic .gemrc config for Ruby setup.
    - Greatly reduces Ruby Gem install time by skipping related
      documentation install and generation.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,6 +18,8 @@ done
 rbenv global $MRI
 
 # Ruby
+printf "%s\n" "---" > "$HOME/.gemrc.test"
+printf "%s\n" "gem: --no-document" >> "$HOME/.gemrc.test"
 git clone git://github.com/bkuhlmann/ruby_setup.git
 (
   cd ruby_setup
